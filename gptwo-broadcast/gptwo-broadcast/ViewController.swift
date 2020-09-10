66137e2cc86cc9bab1781239f7ad2eb385155f84
exert-oneself <1435069584@qq.com>
2020-09-10 20:33:52 +0800

    创建项目

---
+++ gptwo-broadcast/gptwo-broadcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gptwo-broadcast
+//
+//  Created by wgp on 2020/9/10.
+//  Copyright © 2020 wgp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
